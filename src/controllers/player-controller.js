e348796f7335b1559a19d9286f7db82f0089972c
JovanMihic99 <jovanmihic99@gmail.com>
2024-11-19 13:59:44 +0100

    feat: add getPlayerById route and controller

--- src/controllers/player-controller.js
+++ src/controllers/player-controller.js
@@ -11,10 +11,10 @@ const createPlayer = asyncHandler(async (req, res) => {
   }
   const id = uuidv4();
 
-  const insertPlayerQuery = `INSERT INTO players (id, nickname) VALUES ('${id}', '${nickname}')`;
+  const query = `INSERT INTO players (id, nickname) VALUES ('${id}', '${nickname}')`;
   try {
     // Run insert query
-    await runQuery(db, insertPlayerQuery);
+    await runQuery(db, query);
     // console.log(`Player ${nickname} inserted into players table`);
     res.status(200).json({
       message: `Player ${nickname} with id: ${id} inserted into players table`,
@@ -26,6 +26,23 @@ const createPlayer = asyncHandler(async (req, res) => {
 
 const getPlayerById = asyncHandler(async (req, res) => {
   const id = req.params.id;
+  let result;
+  const query = `SELECT * FROM players WHERE id = ?`;
+  try {
+    db.get(query, [id], (err, row) => {
+      if (err) {
+        console.error("Error executing query:", err.message);
+      } else {
+        result = row;
+        console.log("Player data:", row);
+        res.status(200).json({
+          message: result,
+        });
+      }
+    });
+  } catch (err) {
+    res.status(500).json({ message: `Error executing query: ${err.message}` });
+  }
 });
 
-export default { createPlayer };
+export default { createPlayer, getPlayerById };
--- src/routes/player-router.js
+++ src/routes/player-router.js
@@ -3,5 +3,6 @@ import playerController from "../controllers/player-controller.js";
 const playerRouter = express.Router();
 
 playerRouter.post("/create", playerController.createPlayer);
+playerRouter.get("/:id", playerController.getPlayerById);
 
 export default playerRouter;

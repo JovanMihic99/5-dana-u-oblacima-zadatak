678eb303acf78681acdebcbfbd31f4d2e70264eb
JovanMihic99 <jovanmihic99@gmail.com>
2024-11-19 14:44:45 +0100

    feat: add nickname validation

--- src/controllers/player-controller.js
+++ src/controllers/player-controller.js
@@ -1,38 +1,49 @@
 import asyncHandler from "express-async-handler";
 import { v4 as uuidv4 } from "uuid";
-import sqlite3 from "sqlite3";
-// import dbPromise from "../../db/database.js";
-import { runQuery, db } from "../../db/db.js";
+import { db } from "../../db/db.js";
+
 const createPlayer = asyncHandler(async (req, res) => {
   const { nickname } = req.body;
-  if (!nickname) {
-    res.status(400);
-    throw new Error("Player nickname is required");
-  }
-  const id = uuidv4();
 
-  const query = `INSERT INTO players (id, nickname) VALUES (? , ?)`;
-  db.run(query, [id, nickname], function (err) {
+  // Nickname validation (rework this later)
+  if (!nickname) {
+    return res.status(400).json({ message: `Error: Nickname is required` });
+  }
+  const selectNicknameQuery = `SELECT * FROM players WHERE nickname = ?`;
+  db.get(selectNicknameQuery, [nickname], (err, row) => {
     if (err) {
       return res.status(500).json({ error: err.message });
     }
-    res.status(200).json({
-      id,
-      nickname,
-      wins: 0,
-      loses: 0,
-      elo: 0,
-      hoursPlayed: 0,
-      ratingAdjustment: null,
-      teamId: null,
+    if (row) {
+      return res.status(400).json({
+        message: `Error: Nickname already taken, please choose a different one`,
+      });
+    }
+
+    const id = uuidv4();
+    // INSERT query
+    const query = `INSERT INTO players (id, nickname) VALUES (? , ?)`;
+    db.run(query, [id, nickname], function (err) {
+      if (err) {
+        return res.status(500).json({ error: err.message });
+      }
+      return res.status(200).json({
+        id,
+        nickname,
+        wins: 0,
+        loses: 0,
+        elo: 0,
+        hoursPlayed: 0,
+        ratingAdjustment: null,
+        teamId: null,
+      });
     });
   });
 });
-
 const getPlayerById = asyncHandler(async (req, res) => {
   const { id } = req.params;
+  // SELECT query
   const selectQuery = `SELECT * FROM players WHERE id = ?`;
-
   db.get(selectQuery, [id], (err, row) => {
     if (err) {
       return res.status(500).json({ error: err.message });
@@ -40,7 +51,7 @@ const getPlayerById = asyncHandler(async (req, res) => {
     if (!row) {
       return res.status(400).json({ error: "Player not found" });
     }
-    res.status(200).json(row);
+    return res.status(200).json(row);
   });
 });
 
